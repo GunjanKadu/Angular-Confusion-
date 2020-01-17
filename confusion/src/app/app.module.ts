0fca9391973626dc47a4a14f4d1f903196b74be4
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-01-17 16:44:29 +0100

    Angular Routing

--- confusion/src/app/app.module.ts
+++ confusion/src/app/app.module.ts
@@ -1,3 +1,16 @@
+import "hammerjs";
+
+import { DishService } from "./services/dish.service";
+
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./menu/menu.component";
+import { DishDetailComponent } from "./dish-detail/dish-detail.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -7,18 +20,19 @@ import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { MenuComponent } from "./menu/menu.component";
-
-import "hammerjs";
-import { DishDetailComponent } from "./dish-detail/dish-detail.component";
-import { DishService } from "./services/dish.service";
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from "./app-routing/app-routing.module";
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishDetailComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
